92ca08a4d625bd7a0a54d8f63bb2c1ae49585dba
André Souza <an3dree@gmail.com>
2025-10-09 10:47:21 -0300

    feat: SEO otimization

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,26 @@
 const ProfileSection: React.FC = () => {
   return (
-      <section className="profile-section">
+      <section className="profile-section" itemScope itemType="https://schema.org/Person">
 
-        <img src="https://avatars.githubusercontent.com/u/55762057?v=4" alt="André Souza" className="profile-pic" />{" "} 
+      <h1 className="hidden-but-accessible">
+      André Souza (an3dree) - Backend Developer
+      </h1>
+
+        <img
+          src="https://avatars.githubusercontent.com/u/55762057?v=4"
+          alt="André Souza - Backend Developer"
+          className="profile-pic" />
 
         <div className="title-container">
 
           <span>Hi, i'm  ✌🏾 </span>
-          <span className="name">André Souza</span>       
-          <span className="title">Backend Developer</span>
-          <span className="sub-title">sometimes I teach programing, sometimes I do DevOps and Frontend</span>
+          <span className="name" itemProp="name">André Souza</span>
+          <span className="title" itemProp="jobTitle">Backend Developer</span>
+          <span className="sub-title" itemProp="description">sometimes I teach programing, sometimes I do DevOps and Frontend</span>
+
+          <meta itemProp="alternateName" content="an3dree" />
+          <meta itemProp="url" content="https://an3dree.dev" />
+
         </div>
 
       </section>
