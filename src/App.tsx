bb42f08bf8bad8fbcfe90fb95fa6b80c0c5f2dd1
André Souza <an3dree@gmail.com>
2025-10-09 03:02:06 -0300

    deploy to github

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar.tsx'
+import ProfileSection from './components/Profile.tsx'
+import GitHubStatus from './components/GitHubStatus.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section className="profile-section">
-
-<img src="https://avatars.githubusercontent.com/u/55762057?v=4" alt="André Souza" className="profile-pic" />{" "} <div className="title-container">
-
-        
-          <span>Hi, i'm  ✌🏾 </span>
-          <span className="name">André Souza</span>
-      
-       
-        <span className="title">Backend Developer</span>
-        <span className="sub-title">sometimes I teach programing, sometimes I do DevOps and Frontend</span>
-      </div>
-
-      </section>
-         
+      <Navbar />
+      <main>
+        <ProfileSection />
+        <GitHubStatus />
+      </main>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/">@an3dree</a>
+      </div>
+      <div className="navbar-links">
+        <a href="#">/projects</a>
+        <a href="#">/skills</a>
+        <a href="#">/carreer</a>
+      </div>
+    </nav>
+  );
+}
