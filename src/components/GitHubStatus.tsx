e73aef0887700cc2e0b2a4c0d8b2579f1b0eb7ff
André Souza <an3dree@gmail.com>
2025-10-09 09:53:51 -0300

    fix: github username

--- src/components/GitHubStatus.tsx
+++ src/components/GitHubStatus.tsx
@@ -5,13 +5,13 @@ export default function GitHubStats() {
       <div className="stats-container">
        <img 
           height="180em"
-          src="https://github-readme-stats.vercel.app/api?username=Andre-Souza-Dev&show_icons=true&include_all_commits=true&hide_border=true&title_color=61dafb&icon_color=61dafb&text_color=e0e0e0&bg_color=282c34"
+          src="https://github-readme-stats.vercel.app/api?username=an3dree&show_icons=true&include_all_commits=true&hide_border=true&title_color=61dafb&icon_color=61dafb&text_color=e0e0e0&bg_color=282c34"
           alt="André Souza's GitHub Stats"
         /> 
 
         <img 
           height="180em"
-          src="https://github-readme-stats.vercel.app/api/top-langs/?username=Andre-Souza-Dev&layout=compact&langs_count=7&hide_border=true&title_color=61dafb&text_color=e0e0e0&bg_color=282c34"
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=an3dree&layout=compact&langs_count=7&hide_border=true&title_color=61dafb&text_color=e0e0e0&bg_color=282c34"
           alt="André Souza's Top Languages"
         />
       </div>
