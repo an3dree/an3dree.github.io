d8e7a2d7b0647ee6d900f5b2ca5418f0b3fae13a
André Souza <an3dree@gmail.com>
2025-10-13 20:12:27 -0300

    fix: github status size

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,31 +1,38 @@
 const ProfileSection: React.FC = () => {
   return (
-      <section className="profile-section" itemScope itemType="https://schema.org/Person">
-
+    <section
+      className="profile-section"
+      itemScope
+      itemType="https://schema.org/Person"
+    >
       <h1 className="hidden-but-accessible">
-      André Souza (an3dree) - Backend Developer
+        André Souza (an3dree) - Backend Developer
       </h1>
 
-        <img
-          src="https://avatars.githubusercontent.com/u/55762057?v=4"
-          alt="André Souza - Backend Developer"
-          className="profile-pic" />
+      <img
+        src="https://avatars.githubusercontent.com/u/55762057?v=4"
+        alt="André Souza - Backend Developer"
+        className="profile-pic"
+        itemProp="image"
+      />
 
-        <div className="title-container">
+      <div className="title-container">
+        <span>Hi, I'm ✌🏾</span>
+        <span className="name" itemProp="name">
+          André Souza
+        </span>
+        <span className="title" itemProp="jobTitle">
+          Backend Developer
+        </span>
+        <span className="sub-title" itemProp="description">
+          sometimes I teach programing, sometimes I do DevOps and Frontend
+        </span>
 
-          <span>Hi, i'm  ✌🏾 </span>
-          <span className="name" itemProp="name">André Souza</span>
-          <span className="title" itemProp="jobTitle">Backend Developer</span>
-          <span className="sub-title" itemProp="description">sometimes I teach programing, sometimes I do DevOps and Frontend</span>
-
-          <meta itemProp="alternateName" content="an3dree" />
-          <meta itemProp="url" content="https://an3dree.dev" />
-
-        </div>
-
-      </section>
-
+        <meta itemProp="alternateName" content="an3dree" />
+        <meta itemProp="url" content="https://an3dree.dev" />
+      </div>
+    </section>
   );
-}
+};
 
 export default ProfileSection;
