08aca87ef835a04e13896e81310eba240a416b70
André Souza <an3dree@gmail.com>
2025-11-14 19:59:19 -0300

    epione

--- src/components/CareerPage.tsx
+++ src/components/CareerPage.tsx
@@ -29,7 +29,9 @@ export default function CareerPage() {
                 alignItems: "flex-start"
             }}>
                 <h2>epione | 2020 ~ 2021 </h2>
+                <hr />
                 <span>out/2020 ~ apr/2021 - 6 months</span>
+                <h3>Full Stack Dev</h3>
                 <p style={{ textAlign: "left", color: "white" }}>
                     epione is a health startup where i do my intership. There i worked on developing health-related applications and improving user experience.
                     Technologies I used there were mainly modern Angular and some backend with Node.js.
